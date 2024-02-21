85b03f7e119045a85036a769688741a883267e78
vraicu <veronica.cojocaru11@gmail.com>
2024-02-21 10:59:29 +0100

    seed search db via http req to auctions service

--- src/SearchService/Data/DbInitializer.cs
+++ src/SearchService/Data/DbInitializer.cs
@@ -2,6 +2,7 @@ using System.Text.Json;
 using MongoDB.Driver;
 using MongoDB.Entities;
 using SearchService.Models;
+using SearchService.Services;
 
 namespace SearchService.Data;
 
@@ -16,14 +17,25 @@ public class DbInitializer
             .Key(x => x.Color, KeyType.Text)
             .CreateAsync();
 
-        var count = await DB.CountAsync<Item>();
-        if (count == 0)
-        {
-            Console.WriteLine("No data - will attempt to seed");
-            var itemData = await File.ReadAllTextAsync("Data/auctions.json");
-            var options = new JsonSerializerOptions { PropertyNameCaseInsensitive = true };
-            var items = JsonSerializer.Deserialize<List<Item>>(itemData, options);
+        // var count = await DB.CountAsync<Item>();
+        // if (count == 0)
+        // {
+        //     Console.WriteLine("No data - will attempt to seed");
+        //     var itemData = await File.ReadAllTextAsync("Data/auctions.json");
+        //     var options = new JsonSerializerOptions { PropertyNameCaseInsensitive = true };
+        //     var items = JsonSerializer.Deserialize<List<Item>>(itemData, options);
 
+        //     await DB.SaveAsync(items);
+        // }
+
+        using var scope = app.Services.CreateScope();
+        var httpClient = scope.ServiceProvider.GetRequiredService<AuctionSvcHttpClient>();
+        var items = await httpClient.GetItemsForSearchDb();
+
+        Console.WriteLine($"{items.Count} returned from the auction service");
+
+        if (items.Count > 0)
+        {
             await DB.SaveAsync(items);
         }
     }

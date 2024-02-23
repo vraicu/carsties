6803e7675661e03c9e21d693999fe7ba92ff5359
vraicu <veronica.cojocaru11@gmail.com>
2024-02-23 10:59:29 +0100

    add new identity svc client

--- src/IdentityService/Config.cs
+++ src/IdentityService/Config.cs
@@ -56,6 +56,18 @@ public static class Config
                 RedirectUris = {"https://www.getpostman.com/oauth2/callback"},
                 ClientSecrets = {new Secret("NotASecret".Sha256())},
                 AllowedGrantTypes = {GrantType.ResourceOwnerPassword}
+            },
+            new Client
+            {
+                ClientId = "nextApp",
+                ClientName = "nextApp",
+                ClientSecrets = {new Secret("secrets".Sha256())},
+                AllowedGrantTypes = GrantTypes.CodeAndClientCredentials,
+                RequirePkce = false,
+                RedirectUris = {"http://localhost:3000/api/auth/callback/id-server"},
+                AllowOfflineAccess = true,
+                AllowedScopes = {"openId", "profile", "auctionApp"},
+                AccessTokenLifetime = 3600*24*30
             }
         };
 }

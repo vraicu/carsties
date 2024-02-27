783b58d063a9cf56c40fc5e9af77a3dc2ba95ac5
vraicu <veronica.cojocaru11@gmail.com>
2024-02-27 13:46:37 +0100

    add auction cards + pagination

---
+++ frontend/web-app/app/auctions/Listings.tsx
@@ -0,0 +1,51 @@
+'use client'
+
+import React, { useEffect, useState } from "react";
+import AuctionCard from "./AuctionCard";
+import AppPagination from "@/components/AppPagination";
+import { Auction, PagedResult } from "@/types";
+import { getData } from "../actions/auctionActions";
+import Filters from "./Filters";
+import { useParamsStore } from "@/hooks/useParamsStore";
+import { shallow } from "zustand/shallow";
+import qs from 'query-string';
+
+export default function Listings() {
+    const [data, setData] = useState<PagedResult<Auction>>();
+    const params = useParamsStore(state => ({
+        pageNumber: state.pageNumber,
+        pageSize : state.pageSize,
+        searchTerms: state.searchTerm
+    }), shallow);
+    const setParams = useParamsStore(state => state.setParams)
+    const url = qs.stringifyUrl({
+        url: '', query: params
+    })
+
+    function setPageNumber(pageNumber: number) {
+        setParams({pageNumber})
+    }
+
+    useEffect(() => { 
+        getData(url).then(data => {
+            setData(data)
+        })
+    }, [url])
+
+    if (!data) return <h3>Loading...</h3>
+
+    return (
+        <>
+            <Filters/>
+            <div className="grid grid-cols-4 gap-6">
+                {data.results.map(auction => (
+                    <AuctionCard auction={auction} key={auction.id}/>
+                ))}
+            </div>
+            <div className="flex justify-center mt-4">
+                <AppPagination currentPage={params.pageNumber} pageCount={data.pageCount} pageChanged={setPageNumber}/>
+            </div>
+        </>
+
+    )
+}
--- frontend/web-app/app/page.tsx
+++ frontend/web-app/app/page.tsx
@@ -1,7 +1,9 @@
+import Listings from "./auctions/Listings";
+
 export default function Home() {
   return(
     <div>
-      <h3 className="text-3xl font-semibold">Content goes here!</h3>
+      <Listings/>
     </div>
   )
 }

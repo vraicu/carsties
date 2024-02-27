5edd8a4228786b738f40b657a78885de544ecc3e
vraicu <veronica.cojocaru11@gmail.com>
2024-02-27 14:56:43 +0100

    add empty states

--- frontend/web-app/app/auctions/Listings.tsx
+++ frontend/web-app/app/auctions/Listings.tsx
@@ -1,4 +1,4 @@
-'use client'
+"use client";
 
 import React, { useEffect, useState } from "react";
 import AuctionCard from "./AuctionCard";
@@ -8,46 +8,60 @@ import { getData } from "../actions/auctionActions";
 import Filters from "./Filters";
 import { useParamsStore } from "@/hooks/useParamsStore";
 import { shallow } from "zustand/shallow";
-import qs from 'query-string';
+import qs from "query-string";
+import EmptyFilter from "@/components/EmptyFilter";
 
 export default function Listings() {
-    const [data, setData] = useState<PagedResult<Auction>>();
-    const params = useParamsStore(state => ({
-        pageNumber: state.pageNumber,
-        pageSize : state.pageSize,
-        searchTerm: state.searchTerm,
-        orderBy: state.orderBy,
-        filterBy: state.filterBy
-    }), shallow);
-    const setParams = useParamsStore(state => state.setParams)
-    const url = qs.stringifyUrl({
-        url: '', query: params
-    })
+  const [data, setData] = useState<PagedResult<Auction>>();
+  const params = useParamsStore(
+    (state) => ({
+      pageNumber: state.pageNumber,
+      pageSize: state.pageSize,
+      searchTerm: state.searchTerm,
+      orderBy: state.orderBy,
+      filterBy: state.filterBy,
+    }),
+    shallow
+  );
+  const setParams = useParamsStore((state) => state.setParams);
+  const url = qs.stringifyUrl({
+    url: "",
+    query: params,
+  });
 
-    function setPageNumber(pageNumber: number) {
-        setParams({pageNumber})
-    }
+  function setPageNumber(pageNumber: number) {
+    setParams({ pageNumber });
+  }
 
-    useEffect(() => { 
-        getData(url).then(data => {
-            setData(data)
-        })
-    }, [url])
+  useEffect(() => {
+    getData(url).then((data) => {
+      setData(data);
+    });
+  }, [url]);
 
-    if (!data) return <h3>Loading...</h3>
+  if (!data) return <h3>Loading...</h3>;
 
-    return (
+  return (
+    <>
+      <Filters />
+      {data.totalCount === 0 ? (
+        <EmptyFilter showReset={true} />
+      ) : (
         <>
-            <Filters/>
-            <div className="grid grid-cols-4 gap-6">
-                {data.results.map(auction => (
-                    <AuctionCard auction={auction} key={auction.id}/>
-                ))}
-            </div>
-            <div className="flex justify-center mt-4">
-                <AppPagination currentPage={params.pageNumber} pageCount={data.pageCount} pageChanged={setPageNumber}/>
-            </div>
+          <div className="grid grid-cols-4 gap-6">
+            {data.results.map((auction) => (
+              <AuctionCard auction={auction} key={auction.id} />
+            ))}
+          </div>
+          <div className="flex justify-center mt-4">
+            <AppPagination
+              currentPage={params.pageNumber}
+              pageCount={data.pageCount}
+              pageChanged={setPageNumber}
+            />
+          </div>
         </>
-
-    )
+      )}
+    </>
+  );
 }
